0d844b79b49f0e1d709e931cf5864dfdfc52c4b9
Rishabh Agrawal <r.agrawal@dyninno.in>
2023-06-27 15:23:40 +0530

    create layout using flexbox

--- App.js
+++ App.js
@@ -1,28 +1,34 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.dummyText}>Hello Madhav!!!!!!!!!</Text>
-      <Text style={styles.dummyText}>Hello Rishabh!!!!!!!!!!</Text>
-      <Button title="Click Me" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.TextInput} placeholder="Your course goal" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals..</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    paddingTop: 50,
+    paddingBottom: 50,
+    paddingLeft: 20,
+    paddingRight: 20,
   },
-  dummyText: {
-    color: "red",
-    margin: 10,
-    borderWidth: 1,
-    borderColor: "red",
-    borderStyle: "solid",
-    padding: 10,
+  inputContainer : {
+    flexDirection: "row",
+    justifyContent: 'space-between'
   },
-});
+  TextInput: {
+    borderWidth:1,
+    borderColor: '#ccc',
+    width: '80%',
+    paddingLeft: 10
+  }
+})
