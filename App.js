64727c660376c2f5cbd4d340aab61aad0462ded1
Rishabh Agrawal <r.agrawal@dyninno.in>
2023-06-27 16:07:26 +0530

    improve layout for todo

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
         <TextInput style={styles.TextInput} placeholder="Your course goal" />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalContainer}>
         <Text>List of goals..</Text>
       </View>
     </View>
@@ -20,15 +20,26 @@ const styles = StyleSheet.create({
     paddingBottom: 50,
     paddingLeft: 20,
     paddingRight: 20,
+    flex:1
   },
   inputContainer : {
     flexDirection: "row",
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    flex: 1
   },
   TextInput: {
     borderWidth:1,
     borderColor: '#ccc',
     width: '80%',
-    paddingLeft: 10
+    paddingLeft: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  goalContainer : {
+    flex:6
   }
 })
